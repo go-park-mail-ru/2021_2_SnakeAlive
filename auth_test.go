a5e4396a08e66c062ee63ade189e484a86610b29
kravtandr <kravtandr@gmail.com>
2021-09-25 21:35:02 +0300

    tests auth and places

--- auth_test.go
+++ auth_test.go
@@ -1,95 +1,89 @@
 package main
 
 import (
-	"bytes"
-	"context"
-	"fmt"
-	"net"
-	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/valyala/fasthttp"
-	"github.com/valyala/fasthttp/fasthttputil"
 )
 
-// serve serves http request using provided fasthttp handler
-func serve(handler fasthttp.RequestHandler, req *http.Request) (*http.Response, error) {
-	ln := fasthttputil.NewInmemoryListener()
-	defer ln.Close()
-
-	go func() {
-		err := fasthttp.Serve(ln, handler)
-		if err != nil {
-			panic(fmt.Errorf("failed to serve: %v", err))
-		}
-	}()
-
-	client := http.Client{
-		Transport: &http.Transport{
-			DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
-				return ln.Dial()
-			},
-		},
-	}
-
-	return client.Do(req)
-}
-
 type test struct {
-	input  []byte
-	method string
-	want   int
+	input string
+	want  int
 }
 
 func TestLogin(t *testing.T) {
 	tests := []test{
-		{[]byte(`{"name": "name2", "surname": "surname2", "email": "alex@mail.ru", "password": "password"}`), "POST",
+		{(`{"name": "name2", "surname": "surname2", "email": "alex@mail.ru", "password": "password"}`),
 			fasthttp.StatusOK},
-		{[]byte(`{"name": "name2", "surname": "surname2", "email": "xxxxxx@mail.ru", "password": "pass"}`), "POST",
+		{(`{"name": "name2", "surname": "surname2", "email": "alextest@mail.ru", "password": "password"}`),
 			fasthttp.StatusNotFound},
-		{[]byte(`{"name": "name2", "surname": "surname2", "email": "alex@mail.ru", "password": "xxxxxxxx"}`), "POST",
+		{(`{"name": "name2", "surname": "surname2", "email": "alex@mail.ru", "password": "xxxxxxxx"}`),
 			fasthttp.StatusBadRequest},
-		{[]byte(`xxxxx`), "POST", fasthttp.StatusBadRequest},
+		{(`xxxxx`), fasthttp.StatusBadRequest},
 	}
+	var c fasthttp.RequestCtx
+	ctx := (*fasthttp.RequestCtx)(&c)
 
 	for _, tc := range tests {
-		t.Log(tc.input, tc.want)
-		r, err := http.NewRequest(tc.method, "http://:8080", bytes.NewBuffer(tc.input))
-		if err != nil {
-			t.Error(err)
-		}
-		res, err := serve(login, r)
-		if err != nil {
-			t.Error(err)
-		}
-		assert.Equal(t, tc.want, res.StatusCode)
+		ctx.Request.SetBody(nil)
+		ctx.Request.AppendBody([]byte(tc.input))
+		login(ctx)
+		assert.Equal(t, tc.want, ctx.Response.Header.StatusCode())
 	}
 }
 
 func TestRegister(t *testing.T) {
 	tests := []test{
-		{([]byte(`{"name": "name2", "surname": "surname2", "email": "asdf@mail.ru", "password": "pass"}`)), "POST",
+		{(`{"name": "name2", "surname": "surname2", "email": "asdf@mail.ru", "password": "pass"}`),
 			fasthttp.StatusOK},
-		{[]byte(`{"name": "name2", "surname": "surname2", "email": "alex@mail.ru", "password": "pass"}`), "POST",
+		{(`{"name": "name2", "surname": "surname2", "email": "alex@mail.ru", "password": "pass"}`),
 			fasthttp.StatusBadRequest},
-		{[]byte(`{"name": "name2", "surname": "surname2", "email": "alex@mail.ru", "xxxxx": "xxxxxxxx"}`), "POST",
+		{(`{"name": "name2", "surname": "surname2", "email": "alex@mail.ru", "xxxxx": "xxxxxxxx"}`),
 			fasthttp.StatusBadRequest},
-		{[]byte(`{"name": "name2", "surname": "surname2", "xxxx": "alex@mail.ru", "password": "xxxxxxxx"}`), "POST",
+		{(`{"name": "name2", "surname": "surname2", "xxxx": "alex@mail.ru", "password": "xxxxxxxx"}`),
 			fasthttp.StatusBadRequest},
-		{[]byte(`xxxxx`), "POST", fasthttp.StatusBadRequest},
+		{(`xxxxx`), fasthttp.StatusBadRequest},
 	}
+	var c fasthttp.RequestCtx
+	ctx := (*fasthttp.RequestCtx)(&c)
 
 	for _, tc := range tests {
-		t.Log(tc.input, tc.want)
-		r, err := http.NewRequest(tc.method, "http://:8080", bytes.NewBuffer(tc.input))
-		if err != nil {
-			t.Error(err)
-		}
-		res, err := serve(registration, r)
-		if err != nil {
-			t.Error(err)
-		}
-		assert.Equal(t, tc.want, res.StatusCode)
+		ctx.Request.SetBody(nil)
+		ctx.Request.AppendBody([]byte(tc.input))
+		registration(ctx)
+		assert.Equal(t, tc.want, ctx.Response.Header.StatusCode())
 	}
+}
+
+type User struct {
+	Name     string `json:"name"`
+	Surname  string `json:"surname"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func TestPlacesList(t *testing.T) {
+
+	tests := []test{
+		{"Russia", fasthttp.StatusOK},
+		{"Germany", fasthttp.StatusNotFound},
+	}
+	var c fasthttp.RequestCtx
+	ctx := (*fasthttp.RequestCtx)(&c)
+
+	ctx.Request.AppendBody([]byte(`{"name": "name2", "surname": "surname2", "email": "alex@mail.ru", "password": "password"}`))
+	login(ctx)
+	assert.Equal(t, fasthttp.StatusOK, ctx.Response.Header.StatusCode())
+
+	placesList(ctx)
+	assert.Equal(t, fasthttp.StatusUnauthorized, ctx.Response.Header.StatusCode())
+
+	for _, tc := range tests {
+		ctx.Request.Header.SetCookie("SnakeAlive", "3259306991")
+		ctx.SetUserValue("name", tc.input)
+		placesList(ctx)
+		assert.Equal(t, tc.want, ctx.Response.Header.StatusCode())
+	}
+
 }
