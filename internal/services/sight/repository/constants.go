9b6d90ca097bef39db2de32268cb56285cea2522
Черных Никита Александрович <nachernyk1@stream.ru>
2021-11-25 12:10:17 +0300

    query fixed

--- internal/services/sight/repository/constants.go
+++ internal/services/sight/repository/constants.go
@@ -2,7 +2,7 @@ package repository
 
 const (
 	GetSightByIdQuery       = `SELECT id, name, country, rating, tags, description, photos, lng, lat FROM Places WHERE id = $1`
-	GetSightsByCountryQuery = `SELECT id, name, tags, photos, country, avg(rating) over (partition by id) as total_rating, lng, lat FORM Places WHERE country = $1 ORDER BY total_rating DESC LIMIT 10`
+	GetSightsByCountryQuery = `SELECT id, name, tags, photos, country, avg(rating) over (partition by id) as total_rating, lng, lat FROM Places WHERE country = $1 ORDER BY total_rating DESC LIMIT 10`
 	GetSightsByIDs          = `SELECT id, name, country, rating, tags, description, photos, lng, lat FROM Places WHERE id = any($1)`
 	SearchSights            = `SELECT id, name, country, rating, tags, description, photos, lng, lat FROM Places WHERE name LIKE $1 SKIP $2 LIMIT $3`
 )
