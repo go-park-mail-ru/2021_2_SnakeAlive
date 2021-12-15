326d3e4f3f20fd8ae0a169637de8641c2417e7cd
Черных Никита Александрович <nachernyk1@stream.ru>
2021-12-15 16:03:39 +0300

    logs added

--- internal/gateway/config/config.go
+++ internal/gateway/config/config.go
@@ -31,7 +31,6 @@ func (c *Config) Setup() error {
 	if err := envconfig.Process("GATEWAY", c); err != nil {
 		return err
 	}
-
 	lgr := logs.BuildLogger()
 	c.Logger = lgr.Logger
 	c.Ctx, c.Cancel = context.WithCancel(context.Background())
